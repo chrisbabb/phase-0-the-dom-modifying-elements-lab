a4a8739b417b6cbd12847c97e9902696beb526ea
Christopher Babb <chrisbabb@DESKTOP-ECJLDP8.localdomain>
2022-05-29 19:14:05 -0700

    commit tree d0459ee912d3184e0cd252a8c9df0110822bf8fa

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+document.body.innerHTML = "<h1 id='victory'><h1>";
+const newHeader = document.getElementById("victory");
+
+newHeader.innerHTML = "Chris is the champion";
